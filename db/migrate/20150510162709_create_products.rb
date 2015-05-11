9350559d3057ef785e4916cc96dfbcadae7675cf
Ojasvi Garg <ogarg@tower-research.com>
2015-05-11 16:44:05 +0530

    fix
    Change-Id: I86734a91458fb316fc97ce85dc8f01456286fc85

--- db/migrate/20150510162709_create_products.rb
+++ db/migrate/20150510162709_create_products.rb
@@ -1,11 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-    create_table :products
+    create_table :products do |t|
       t.string :name
       t.float :price
       t.integer :stock
       t.string :description
-      t.integer :product_category_id
 
       t.timestamps
     end
